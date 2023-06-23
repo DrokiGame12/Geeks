cc416d56da4954b159f8c79c3573c431f2978b7e
DrokiGame <106255964+DrokiGame12@users.noreply.github.com>
2023-06-24 02:02:35 +0600

    Add files via upload

---
+++ HomeWorks/HW-6/app.js
@@ -0,0 +1,24 @@
+const input = document.querySelector('input')
+const increment = document.querySelector('#increment')
+const decrement = document.querySelector('.decrement')
+
+increment.onclick = () => input.value++
+decrement.onclick = () => {
+    if(0 < input.value) {
+        input.value--
+    }
+}
+
+const button = document.querySelectorAll('.button')
+//console.log(button);
+// for(let i = 0; i < button.length; i++){
+//     button[i].addEventListener('click', function(){
+//         console.log('You pressed the button');
+//     })
+// }
+
+button.forEach(i => {
+    i.addEventListener('click', function(){
+        console.log('You pressed the button')
+    })
+})
